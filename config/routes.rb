ba00f3ffbd502434361a46c20b761b691eba6c5c
Owen Baldwin <owenabaldwin@gmail.com>
2021-12-17 20:21:08 +0000

    compositions and banner done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :portfolios
+  # resources :portfolios
+  get :portfolio, to: 'pages#portfolio'
+  get :about, to: 'pages#about'
+  get :references, to: 'pages#references'
+  get :contact, to: 'pages#contact'
+  get :cv, to: 'pages#cv'
 end
